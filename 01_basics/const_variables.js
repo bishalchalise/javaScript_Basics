c77ca806692b4904e85edb3a365dc2120fb2c5f6
bishalchalise <107859484+bishalchalise@users.noreply.github.com>
2023-12-14 16:41:41 +0000

    variables_and_constants

---
+++ 01_basics/const_variables.js
@@ -0,0 +1,29 @@
+let  bishal = 1234.3
+let chalise = 'chalise'
+// console.log(bishal, chalise)
+
+
+var num1 =  1
+var num2 = "ram"
+
+hari = "next name"
+shyam = 2
+
+// var num1 = 'chalose'
+// console.log(num1, num2)
+console.table([typeof num1+" "+num1,typeof num2+" "+num2, typeof chalise+" "+chalise, typeof bishal+" "+bishal, 
+typeof hari+" "+hari,
+typeof shyam+" "+shyam,
+
+] )
+
+
+/* 
+prefer not to use var in javascript because of fucntion scope and block scope
+*/
+
+
+
+
+
+
